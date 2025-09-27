97477ad00b47a68e7bb4678f35f7bfc8f055b109
thetko1996 <thetkomaung966@gmail.com>
2025-09-28 01:38:15 +0900

    feat: Implement initial HomeScreen widget
    Creates the basic structure for the HomeScreen widget, including StatefulWidget and initial state.

---
+++ lib/week04/u_and_i/screen/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
