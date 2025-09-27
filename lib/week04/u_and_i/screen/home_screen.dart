6527f8857f69d134209d5b3089cd5cc6ddd0b26c
thetko1996 <thetkomaung966@gmail.com>
2025-09-28 01:50:15 +0900

    Fix: Remove trailing newline in home_screen.dart
    This commit removes an unnecessary trailing newline at the end of the home_screen.dart file, ensuring consistency with coding style guidelines.

--- lib/week04/u_and_i/screen/home_screen.dart
+++ lib/week04/u_and_i/screen/home_screen.dart
@@ -56,9 +56,9 @@ class _HomeScreenState extends State<HomeScreen>{
               ),
               _CoupleImage(),
             ],
-          )
+          ),
         ),
-      ),
+      );
     )
   }
 }
