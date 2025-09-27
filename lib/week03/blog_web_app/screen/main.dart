d88d7f6ed7d85c0d4beb2ede8b51b778466c92fe
thetko1996 <thetkomaung966@gmail.com>
2025-09-28 00:36:15 +0900

    feat: Initialize MaterialApp with HomeScreen for blog app
    This commit initializes the MaterialApp with the HomeScreen as the root widget for the blog web application.

--- lib/week03/blog_web_app/screen/main.dart
+++ lib/week03/blog_web_app/screen/main.dart
@@ -1,1 +1,13 @@
 import 'package:myapp/week03/blog_web_app/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
