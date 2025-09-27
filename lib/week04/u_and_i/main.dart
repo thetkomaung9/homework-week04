aa62621475e1e9200672e4476ca4ead26092acf9
thetko1996 <thetkomaung966@gmail.com>
2025-09-28 01:30:17 +0900

    feat: Define headlineLarge text style in theme

--- lib/week04/u_and_i/main.dart
+++ lib/week04/u_and_i/main.dart
@@ -6,7 +6,14 @@ void main(){
     MaterialApp(
       theme: ThemeData(
         fontFamily: 'sunflower',
-        
+        textTheme: TextTheme(
+          headlineLarge: TextStyle(
+            color: Colors.white,
+            fontSize: 80.0,
+            fontWeight: FontWeight.w700,
+            fontFamily: 'parisienne',
+          )
+        )
 
       )
     )
