6ee95ca772fae5b8d774abb2e40dcfc9a7d1c45b
thetko1996 <thetkomaung966@gmail.com>
2025-09-28 00:57:15 +0900

    feat(home_screen): Initialize PageController for image carousel
    This commit initializes a  within the  to manage the image carousel's page transitions.

--- lib/week03/image_carousel/screen/home_screen.dart
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -6,6 +6,11 @@ class HomeScreen extends StatefulWidget{
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<HomeScreen> createState() =>
+  State<HomeScreen> createState() => _HomeScreenState();
 
+
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+  final PageController pageController = PageController();
 }
