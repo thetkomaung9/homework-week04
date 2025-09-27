115c511dd142b3d134b58ab080ebdd98bf1f00cc
thetko1996 <thetkomaung966@gmail.com>
2025-09-28 01:14:15 +0900

    feat: Implement image carousel with basic home screen
    This commit introduces the image carousel feature with a basic home screen setup.

--- lib/week03/image_carousel/main.dart
+++ lib/week03/image_carousel/main.dart
@@ -1,1 +1,10 @@
 import 'package:myapp/week03/image_carousel/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
