c088724076e94ca37fdf97fc6ef45353082c9f19
thetko1996 <thetkomaung966@gmail.com>
2025-09-28 00:25:15 +0900

    feat: Implement basic HomeScreen with WebViewController

--- lib/week03/blog_web_app/screen/home_screen.dart
+++ lib/week03/blog_web_app/screen/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class HomeScreeen
+class HomeScreen extends StatelessWidget{
+
+  WebViewController webViewController = WebViewController()
+  
+}
