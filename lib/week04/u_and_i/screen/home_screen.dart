7387519379cad0125b8bd63ddfb7c3f1e5a47ed8
thetko1996 <thetkomaung966@gmail.com>
2025-09-28 01:39:15 +0900

    feat(home_screen): Initialize firstDay variable
    Initializes the  variable in the  class with the current date and time.

--- lib/week04/u_and_i/screen/home_screen.dart
+++ lib/week04/u_and_i/screen/home_screen.dart
@@ -8,3 +8,7 @@ class HomeScreen extends StatefulWidget{
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
+
+class _HomeScreenState extends State<HomeScreen>{
+  DateTime firstDay = DateTime.now();
+}
