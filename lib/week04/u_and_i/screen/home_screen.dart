88bb2c1e7f17796f6efad22bf0b14dcf94fe9d14
thetko1996 <thetkomaung966@gmail.com>
2025-09-28 01:43:45 +0900

    Fix: Update firstDay state on date picker change
    Updates the  state variable in the  when the date is changed in the .

--- lib/week04/u_and_i/screen/home_screen.dart
+++ lib/week04/u_and_i/screen/home_screen.dart
@@ -25,7 +25,9 @@ class _HomeScreenState extends State<HomeScreen>{
             child: CupertinoDatePicker(
               mode: CupertinoDatePickerMode.date,
               onDateTimeChanged: (DateTime date) {
-                setState()
+                setState((){
+                  firstDay = date;
+                });
               },
             ),
           ),
